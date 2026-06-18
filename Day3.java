a0b657316ad56e5736a9c55615657cd8a5b43990
Snehalatha Chenna <chennasnehalatha41@gmail.com>
2026-06-18 10:39:49 +0530

    day3 file added

---
+++ Day3.java
@@ -0,0 +1,16 @@
+class Day3{
+    
+    public static void main(String[] args){
+        int[] arr={23,56,89,76,4,6,34};
+        int target=56;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                System.out.println("found index at" + i);
+            }
+            
+        
+        }
+        
+        }
+    }
+
