060372b557e3529eec5013ae9d0a81f7087a0c12
Snehalatha Chenna <chennasnehalatha41@gmail.com>
2026-06-18 23:39:06 +0530

    selectionsort file added

---
+++ Selectionsort.java
@@ -0,0 +1,21 @@
+class Selectionsort{
+    public static void main(String[] args){
+        int[] arr={10,2,20,3,5,9,12,1};
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            int minIndex=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=temp;
+
+        }
+        for(int num:arr){
+            System.out.print(num + " " );
+        }
+    }
+}
