af59701879cb6e589a03da62440fae9ce28a759f
chennasatish7-design <chennasatish7@gmail.com>
2026-06-29 23:15:05 +0530

    operators file added

---
+++ Operators.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Operators{
+    public static void main(String[] args){
+        // Scanner sc=new Scanner(System.in);
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        // System.out.println(a+b);
+        // System.out.println(a%b);
+
+        //Relational operator
+        // if(a==b){
+        //     System.out.println("True");
+        // }else{
+        //     System.out.println("False");
+        // }
+
+        //Logical operator
+        // if(a>5 && a<10){
+        //     System.out.println(1);
+        // }else{
+        //     System.out.println(0);
+        // }
+
+
+        //Ternary operator
+        // int max=(a%2==0)?1:0;
+        // System.out.println(max);
+
+        //type conversion(low datatype convert to high datatype)
+        // float result=(float)a/b;
+        // System.out.println(result);
+
+        //type casting (high datatyape convert to low datatype)
+        double result=2.55;
+        int c= (int) result;
+        System.out.println(c);
+
+
+    }
+}
